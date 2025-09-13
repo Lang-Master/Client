33e1d1218a58c7fe5753e5823db2565a039bc50a
eremie523 <reremie523@gmail.com>
2025-09-13 22:50:53 +0100

    Most Recent Push - Start point of this application

---
+++ app/layout.js
@@ -0,0 +1,28 @@
+// import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata = {
+  title: "Lang Master",
+  description: "AI Language Tutor for Nigerian Languages",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+       <body className="antialiased scroll-smooth">
+        {children}
+      </body>
+    </html>
+  );
+}
